262e9c038ead84200e987f26f607c3c4b662e67d
Brooks Van Buren <woofiewilly@gmail.com>
2016-02-27 12:35:20 -0500

    Fixed comma

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// Send a message to the active tab
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs, {"message": "clicked_browser_action"});
-  });
+	chrome.tabs.query({"windowId": chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
+		console.log("I'm in background.js");
+		chrome.tabs.sendMessage(tabs, {"message": "clicked_browser_action"});
+	});
 });
