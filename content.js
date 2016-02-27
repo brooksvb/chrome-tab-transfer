890390c7491bf05a4b0034177b32b15616095f04
Brooks Van Buren <woofiewilly@gmail.com>
2016-02-27 12:26:26 -0500

    Added random stuff hoping it would do something

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		console.log("I'm in content.js");
 		if( request.message === "clicked_browser_action" ) {
 			console.log(tabs);
 		}
