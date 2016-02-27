d858b4e38431f0a4e68c178185727c824f709db9
Brooks Van Buren <woofiewilly@gmail.com>
2016-02-27 13:18:22 -0500

    Reset everything to chrome tutorial

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	// Send a message to the active tab
-  chrome.tabs.query({currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs, {"message": "clicked_browser_action"});
-  });
-});
--- content.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-		console.log("I'm in content.js");
-		if( request.message === "clicked_browser_action" ) {
-			console.log(tabs);
-		}
-	}
-);
